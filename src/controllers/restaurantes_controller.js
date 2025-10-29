3333266d9cbcd21224a8284965b2ff83e63c8262
Andre07G <eleal087@gmail.com>
2025-10-29 00:08:04 -0500

    feat: :sparkles: Crear controlador de restaurantes Se creó el controlador para manejar las acciones CRUD de restaurantes mediante solicitudes HTTP. by @Andre07g

--- src/controllers/restaurantes_controller.js
+++ src/controllers/restaurantes_controller.js
@@ -1,1 +1,50 @@
-// Inicio
+import { obtenerRestaurantes, obtenerRestaurantePorID, crearRestaurante, actualizarRestaurante, eliminarRestaurante } from "../services/restaurantes_services";
+
+export async function obtenerTodosLosRestaurantes(req, res) {
+    try {
+        const games = await obtenerRestaurantes();
+        res.status(200).json(games);
+    } catch (error) {
+        res.status(500).json({error: "Error al obtener todos los restaurantes"});
+    }
+}
+
+export async function obtenerUnRestaurantePorID(req, res) {
+    try {
+        const id = req.params.id;
+        const game = await obtenerRestaurantePorID(id);
+        if(!game) return res.status(404).json({error: "Restaurante no encontrado"});
+        res.status(200).json(game)
+    } catch (error) {
+        res.status(500).json({error: "Error al obtener el restaurante"});
+    }
+}
+
+export async function crearUnRestaurante(req, res) {
+    try {
+        const result = await crearRestaurante(req.body);
+        res.status(201).json(result)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+export async function actualizarUnRestaurante(req, res) {
+    try {
+        const id = req.params.id;
+        const result = await actualizarRestaurante(id, req.body);
+        res.status(202).json(result);
+    } catch (error) {
+        res.status(404).json({error: error.message})
+    }
+}
+
+export async function eliminarUnRestaurante(req, res) {
+    try {
+        const id = req.params.id;
+        const result = await eliminarRestaurante(id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({error: error.message});
+    }
+}
