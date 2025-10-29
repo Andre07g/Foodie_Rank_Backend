965ffae0ba8e1a7beef2260dd85fd1b57dea69ac
Andre07G <eleal087@gmail.com>
2025-10-28 21:29:44 -0500

    feat: :sparkles: Crear servicio de categorías Se creó el archivo categorias_services.js con las funciones CRUD para gestionar las categorías en MongoDB. by @Andre07g

--- src/services/categorias_services.js
+++ src/services/categorias_services.js
@@ -1,1 +1,35 @@
-// Inicio
+import { obtenerBD } from "../config/db.js";
+import { ObjectId } from "mongodb";
+const COLECCION_CATEGORIAS = "categorias"
+
+
+
+export async function obtenerCategorias(){
+    const db = await obtenerBD()
+    return await db.collection(COLECCION_CATEGORIAS).find().toArray();
+
+}
+
+export async function crearCategoria(data){
+    const {nombre, descripcion} = data;
+
+    const categoria = {nombre, descripcion}
+    const db = await obtenerBD()
+    await db.collection(COLECCION_CATEGORIAS).insertOne(categoria);
+    return {message:"La categoria fue creada correctamente"};
+}
+
+export async function actualizarCategoria(id,data) {
+    const db = await obtenerBD()
+    const result = await db.collection(COLECCION_CATEGORIAS).updateOne({_id:new ObjectId(id)},{$set:data});
+    if(result.matchedCount===0){throw new Error("Categoria no encontrada");}
+    return {message: "Categoria modificada correctamente"};
+}
+
+export async function eliminarCategoria(id){
+    const db = await obtenerBD()
+    const result = await db.collection(COLECCION_CATEGORIAS).deleteOne({_id:new ObjectId(id)});
+    if(result.deletedCount===0){throw new Error("Categoria no encontrada");}
+    return {message:"Categoria eliminada correctamente"}
+
+}
