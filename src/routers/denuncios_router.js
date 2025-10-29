7978b5e34883eee536eb626feb7bd46cda66b712
Andre07G <eleal087@gmail.com>
2025-10-29 03:13:55 -0500

    feat: :sparkles: Crear router de denuncios Se creó el router encargado de manejar las rutas asociadas a los denuncios del sistema. by @Andre07g

--- src/routers/denuncios_router.js
+++ src/routers/denuncios_router.js
@@ -1,1 +1,14 @@
-// Inicio
+import { Router } from "express";
+ import { createDenunciaDTO, updateDenunciaDTO } from "../dtos/denuncios_dto.js";
+ import { validationDTO } from "../middlewares/validationDTO.js";
+import { obtenerTodosLosDenuncios, obtenerUnDenuncioPorID, crearUnDenuncio, actualizarUnDenuncio, eliminarUnDenuncio} from "../controllers/denuncios_controller.js";
+
+const router = Router();
+
+router.get("/", obtenerTodosLosDenuncios);
+router.get("/:id", obtenerUnDenuncioPorID);
+router.post("/", createDenunciaDTO, validationDTO,crearUnDenuncio);
+router.patch("/:id", updateDenunciaDTO, validationDTO, actualizarUnDenuncio);
+router.delete("/:id", eliminarUnDenuncio);
+
+export default router;
