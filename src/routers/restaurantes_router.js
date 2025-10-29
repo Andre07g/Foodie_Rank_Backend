8b082dce2b796ebaf4eeefa30a47a58033ccebdb
Andre07G <eleal087@gmail.com>
2025-10-29 03:14:11 -0500

    feat: :sparkles: Crear router de restaurantes Se creó el router para gestionar todas las rutas relacionadas con los restaurantes. by @Andre07g

--- src/routers/restaurantes_router.js
+++ src/routers/restaurantes_router.js
@@ -1,1 +1,14 @@
-// Inicio
+import { Router } from "express";
+ import { createRestauranteDTO, updateRestauranteDTO } from "../dtos/restaurantes_dto.js";
+ import { validationDTO } from "../middlewares/validationDTO.js";
+import { obtenerTodosLosRestaurantes, obtenerUnRestaurantePorID, actualizarUnRestaurante, crearUnRestaurante, eliminarUnRestaurante } from "../controllers/restaurantes_controller.js";
+
+const router = Router();
+
+router.get("/", obtenerTodosLosRestaurantes);
+router.get("/:id", obtenerUnRestaurantePorID);
+router.post("/", createRestauranteDTO, validationDTO,crearUnRestaurante);
+router.patch("/:id", updateRestauranteDTO, validationDTO,actualizarUnRestaurante);
+router.delete("/:id", eliminarUnRestaurante);
+
+export default router;
