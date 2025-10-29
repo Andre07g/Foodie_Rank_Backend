55c5905bb497bf3b99233bf1d54d1c96582a659b
Andre07G <eleal087@gmail.com>
2025-10-29 03:14:46 -0500

    feat: :triangular_ruler: Configurar uso de routers en el servidor Se importaron y registraron todos los routers creados mediante app.use en server.js para habilitar sus rutas en la API. by @Andre07g

--- src/server.js
+++ src/server.js
@@ -3,12 +3,29 @@
 import express from "express";
 import 'dotenv/config'
 import { conectartBD } from "./config/db.js";
+import categoriasRouter from "./routers/categorias_router.js"; 
+import denunciosRouter from "./routers/denuncios_router.js"; 
+import platosRouter from "./routers/platos_router.js";
+import reseñasRouter from "./routers/reseñas_router.js";
+import restaurantesRouter from "./routers/restaurantes_router.js"; 
+import solicitudesRouter from "./routers/solicitudes_router.js"; 
+import usuariosRouter from "./routers/usuarios_router.js";  
 import cors from 'cors'
 
 //Config
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Uso de rutas
+
+app.use("/categorias",categoriasRouter);
+app.use("/denuncios",denunciosRouter);
+app.use("/platos",platosRouter);
+app.use("/reseñas",reseñasRouter);
+app.use("/restaurantes",restaurantesRouter);
+app.use("/solicitudes",solicitudesRouter);
+app.use("/usuarios",usuariosRouter);
 
 app.get("/health", (req, res)=>{
     res.status(200).json({message: "Backend on"});
