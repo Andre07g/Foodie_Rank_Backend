72b4c6a650c4692681f84802c87969f7d35600ea
Andre07G <eleal087@gmail.com>
2025-10-29 03:14:01 -0500

    feat: :sparkles: Crear router de platos Se creó el router para definir las rutas de gestión de los platos desde la API. by @Andre07g

--- src/routers/platos_router.js
+++ src/routers/platos_router.js
@@ -1,1 +1,14 @@
-// Inicio
+import { Router } from "express";
+ import { createPlatoDTO, updatePlatoDTO } from "../dtos/platos_dto.js";
+ import { validationDTO } from "../middlewares/validationDTO.js";
+import { obtenerTodosLosPlatos, obtenerUnPlato, actualizarUnPlato, crearUnPlato, eliminarUnPlato} from "../controllers/platos_controller.js";
+
+const router = Router();
+
+router.get("/", obtenerTodosLosPlatos);
+router.get("/:id", obtenerUnPlato);
+router.post("/", createPlatoDTO, validationDTO,crearUnPlato);
+router.patch("/:id", updatePlatoDTO, validationDTO, actualizarUnPlato);
+router.delete("/:id", eliminarUnPlato);
+
+export default router;
