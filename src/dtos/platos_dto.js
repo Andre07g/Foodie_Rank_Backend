59dc50780a6f101eec11077bfe03cf8e27ebb7d8
Andre07G <eleal087@gmail.com>
2025-10-28 20:46:17 -0500

    feat: :sparkles: Crear DTO de platos Se creó el DTO de platos para manejar la información de los productos o comidas ofrecidas. by @Andre07g

--- src/dtos/platos_dto.js
+++ src/dtos/platos_dto.js
@@ -1,1 +1,28 @@
-// Inicio
+import { body } from "express-validator";
+
+export const createPlatoDTO = [
+    body("nombre")
+        .isString()
+        .trim()
+        .notEmpty(),
+
+    body("precio")
+        .isFloat({ min: 1 })
+        .withMessage("El precio mínimo es 1"),
+
+    body("descripcion")
+        .isString()
+        .trim()
+        .isLength({ min: 5 }),
+
+    body("imagen")
+        .isString()
+        .trim()
+        .notEmpty(),
+
+    body("restaurante")
+        .isMongoId()
+        .withMessage("El restaurante debe ser un ObjectId válido"),
+];
+
+export const updatePlatoDTO = createPlatoDTO.map(val => val.optional());
