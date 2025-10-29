b9ef1a413e31fe6598669c0bb6165a3d39beee78
Andre07G <eleal087@gmail.com>
2025-10-28 20:45:58 -0500

    feat: :sparkles: Crear DTO de categorías Se creó el DTO de categorías para estructurar y validar la información de las categorías en el sistema. by @Andre07g

--- src/dtos/categorias_dto.js
+++ src/dtos/categorias_dto.js
@@ -1,1 +1,30 @@
-// Inicio
+
+import { body } from "express-validator";
+
+export const createCategoriaDTO = [
+    body("nombre")
+        .isString().withMessage("El nombre debe ser texto")
+        .trim()
+        .notEmpty().withMessage("El nombre es obligatorio"),
+
+    body("descripcion")
+        .isString().withMessage("La descripción debe ser texto")
+        .trim()
+        .notEmpty().withMessage("La descripción es obligatoria")
+        .isLength({ min: 5 }).withMessage("Mínimo 5 caracteres en la descripción"),
+];
+
+export const updateCategoriaDTO = [
+    body("nombre")
+        .optional()
+        .isString().withMessage("El nombre debe ser texto")
+        .trim()
+        .notEmpty().withMessage("El nombre no puede estar vacío"),
+
+    body("descripcion")
+        .optional()
+        .isString().withMessage("La descripción debe ser texto")
+        .trim()
+        .notEmpty().withMessage("La descripción no puede estar vacía")
+        .isLength({ min: 5 }).withMessage("Mínimo 5 caracteres en la descripción"),
+];
