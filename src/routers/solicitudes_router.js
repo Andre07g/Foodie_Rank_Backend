f33a4fbbf364aaecb33ae7c631219aa4db194932
Andre07G <eleal087@gmail.com>
2025-10-29 03:14:16 -0500

    feat: :sparkles: Crear router de solicitudes Se creó el router encargado de manejar las rutas CRUD para solicitudes dentro de la aplicación. by @Andre07g

--- src/routers/solicitudes_router.js
+++ src/routers/solicitudes_router.js
@@ -1,1 +1,14 @@
-// Inicio
+import { Router } from "express";
+ import { createSolicitudDTO, updateSolicitudDTO } from "../dtos/solicitudes_dto.js";
+ import { validationDTO } from "../middlewares/validationDTO.js";
+import { obtenerTodasLasSolicitudes, obtenerUnaSolicitudPorID, crearUnaSolicitud, actualizarUnaSolicitud, eliminarUnaSolicitud } from "../controllers/solicitudes_controller.js";
+
+const router = Router();
+
+router.get("/", obtenerTodasLasSolicitudes);
+router.get("/:id", obtenerUnaSolicitudPorID);
+router.post("/", createSolicitudDTO, validationDTO,crearUnaSolicitud);
+router.patch("/:id", updateSolicitudDTO, validationDTO,actualizarUnaSolicitud);
+router.delete("/:id", eliminarUnaSolicitud);
+
+export default router;
