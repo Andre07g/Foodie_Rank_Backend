173b56c41cfc36f78898ef8052e8e45db21ba4d3
Andre07G <eleal087@gmail.com>
2025-10-28 21:29:51 -0500

    feat: :sparkles: Crear servicio de denuncios Se creó el archivo denuncios_services.js con las operaciones CRUD para manejar los denuncios almacenados en MongoDB. by @Andre07g

--- src/services/denuncios_services.js
+++ src/services/denuncios_services.js
@@ -1,1 +1,41 @@
-// Inicio
+import { obtenerBD } from "../config/db.js";
+import { ObjectId } from "mongodb";
+const COLECCION_DENUNCIOS = "denuncios"
+
+
+
+export async function obtenerDenuncios(){
+    const db = await obtenerBD()
+    return await db.collection(COLECCION_DENUNCIOS).find().toArray();
+
+}
+
+export async function obtenerDenuncioPorID(id){
+    const db = await getDB()
+    const result = await db.collection(COLECCION_DENUNCIOS).findOne({_id:new ObjectId(id)});
+    return result;
+}
+
+export async function crearDenuncio(data){
+    const {reseña} = data;
+
+    const Denuncio = {reseña}
+    const db = await obtenerBD()
+    await db.collection(COLECCION_DENUNCIOS).insertOne(Denuncio);
+    return {message:"El Denuncio fue creado correctamente"};
+}
+
+export async function actualizarDenuncio(id,data) {
+    const db = await obtenerBD()
+    const result = await db.collection(COLECCION_DENUNCIOS).updateOne({_id:new ObjectId(id)},{$set:data});
+    if(result.matchedCount===0){throw new Error("Denuncio no encontrado");}
+    return {message: "Denuncio modificado correctamente"};
+}
+
+export async function eliminarDenuncio(id){
+    const db = await obtenerBD()
+    const result = await db.collection(COLECCION_DENUNCIOS).deleteOne({_id:new ObjectId(id)});
+    if(result.deletedCount===0){throw new Error("Denuncio no encontrado");}
+    return {message:"Denuncio eliminado correctamente"}
+
+}
