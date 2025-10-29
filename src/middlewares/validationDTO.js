c5723b8c58059501d445cbfcfbfd0ebce37fab36
Andre07G <eleal087@gmail.com>
2025-10-28 20:48:28 -0500

    feat: :sparkles: Crear validationDTO.js como middleware Se creó el archivo validationDTO.js para validar los datos recibidos en las solicitudes mediante middleware. by @Andre07g

--- src/middlewares/validationDTO.js
+++ src/middlewares/validationDTO.js
@@ -1,1 +1,10 @@
-// Inicio
+import { validationResult } from "express-validator";
+
+export function validationDTO (req, res, next){
+    const result = validationResult(req);
+    if (!result.isEmpty()){
+        res.status(400).send({errors: result.array()});
+        return
+    }
+    next()
+}
