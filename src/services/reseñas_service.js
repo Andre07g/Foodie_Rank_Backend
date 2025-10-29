c9afd19f3dc93145c9b158a4b88e378007fc5ced
Andre07G <eleal087@gmail.com>
2025-10-28 21:30:07 -0500

    feat: :sparkles: Crear servicio de reseñas Se creó el archivo reseñas_services.js con las funciones CRUD para administrar reseñas de usuarios en MongoDB. by @Andre07g

--- src/services/reseñas_service.js
+++ src/services/reseñas_service.js
@@ -1,1 +1,41 @@
-// Inicio
+import { obtenerBD } from "../config/db.js";
+import { ObjectId } from "mongodb";
+const COLECCION_RESEÑAS = "reseñas"
+
+
+
+export async function obtenerReseñas(){
+    const db = await obtenerBD()
+    return await db.collection(COLECCION_RESEÑAS).find().toArray();
+
+}
+
+export async function obtenerReseñaPorID(id){
+    const db = await getDB()
+    const result = await db.collection(COLECCION_RESEÑAS).findOne({_id:new ObjectId(id)});
+    return result;
+}
+
+export async function crearReseña(data){
+    const {usuario, restaurante, calificacion, comentario, likes, dislikes} = data;
+
+    const Reseña = {usuario, restaurante, calificacion, comentario, likes, dislikes}
+    const db = await obtenerBD()
+    await db.collection(COLECCION_RESEÑAS).insertOne(Reseña);
+    return {message:"La Reseña fue creada correctamente"};
+}
+
+export async function actualizarReseña(id,data) {
+    const db = await obtenerBD()
+    const result = await db.collection(COLECCION_RESEÑAS).updateOne({_id:new ObjectId(id)},{$set:data});
+    if(result.matchedCount===0){throw new Error("Reseña no encontrada");}
+    return {message: "Reseña modificada correctamente"};
+}
+
+export async function eliminarReseña(id){
+    const db = await obtenerBD()
+    const result = await db.collection(COLECCION_RESEÑAS).deleteOne({_id:new ObjectId(id)});
+    if(result.deletedCount===0){throw new Error("Reseña no encontrada");}
+    return {message:"Reseña eliminada correctamente"}
+
+}
