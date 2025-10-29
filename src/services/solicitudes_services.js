bed1c79419caaca541682fc067cf29097f0cec2f
Andre07G <eleal087@gmail.com>
2025-10-28 21:30:20 -0500

    feat: :sparkles: Crear servicio de solicitudes Se creó el archivo solicitudes_services.js con la lógica CRUD para gestionar solicitudes dentro del sistema usando MongoDB. by @Andre07g

--- src/services/solicitudes_services.js
+++ src/services/solicitudes_services.js
@@ -1,1 +1,40 @@
-// Inicio
+import { obtenerBD } from "../config/db.js";
+import { ObjectId } from "mongodb";
+const COLECCION_SOLICITUDES = "solicitudes"
+
+
+export async function obtenerSolicitudes(){
+    const db = await obtenerBD()
+    return await db.collection(COLECCION_SOLICITUDES).find().toArray();
+
+}
+
+export async function obtenerSolicitudPorID(id){
+    const db = await getDB()
+    const result = await db.collection(COLECCION_SOLICITUDES).findOne({_id:new ObjectId(id)});
+    return result;
+}
+
+export async function crearSolicitud(data){
+    const {nombre, ubicacion, imagen, popularidad, categoria} = data;
+
+    const Solicitud = {nombre, ubicacion, imagen, popularidad, categoria}
+    const db = await obtenerBD()
+    await db.collection(COLECCION_SOLICITUDES).insertOne(Solicitud);
+    return {message:"El Solicitud fue creada correctamente"};
+}
+
+export async function actualizarSolicitud(id,data) {
+    const db = await obtenerBD()
+    const result = await db.collection(COLECCION_SOLICITUDES).updateOne({_id:new ObjectId(id)},{$set:data});
+    if(result.matchedCount===0){throw new Error("Solicitud no encontrada");}
+    return {message: "Solicitud modificada correctamente"};
+}
+
+export async function eliminarSolicitud(id){
+    const db = await obtenerBD()
+    const result = await db.collection(COLECCION_SOLICITUDES).deleteOne({_id:new ObjectId(id)});
+    if(result.deletedCount===0){throw new Error("Solicitud no encontrada");}
+    return {message:"Solicitud eliminada correctamente"}
+
+}
