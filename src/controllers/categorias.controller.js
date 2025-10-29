931971573adea4b6cab2c44852d887ff8beb08de
Andre07G <eleal087@gmail.com>
2025-10-29 00:07:45 -0500

    feat: :sparkles: Crear controlador de categorías Se creó el controlador para manejar las solicitudes HTTP relacionadas con la gestión de categorías. by @Andre07g

--- src/controllers/categorias.controller.js
+++ src/controllers/categorias.controller.js
@@ -1,1 +1,39 @@
-// Inicio
+import { obtenerCategorias, actualizarCategoria, eliminarCategoria, crearCategoria } from "../services/categorias_services";
+
+export async function obtenerTodasLasCategorias(req, res) {
+    try {
+        const games = await obtenerCategorias();
+        res.status(200).json(games);
+    } catch (error) {
+        res.status(500).json({error: "Error al obtener todas las categorias"});
+    }
+}
+
+export async function crearUnaCategoria(req, res) {
+    try {
+        const result = await crearCategoria(req.body);
+        res.status(201).json(result)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+export async function actualizarUnaCategoria(req, res) {
+    try {
+        const id = req.params.id;
+        const result = await actualizarCategoria(id, req.body);
+        res.status(202).json(result);
+    } catch (error) {
+        res.status(404).json({error: error.message})
+    }
+}
+
+export async function eliminarUnaCategoria(req, res) {
+    try {
+        const id = req.params.id;
+        const result = await eliminarCategoria(id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({error: error.message});
+    }
+}
