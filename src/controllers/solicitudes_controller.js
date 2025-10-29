53fb74cf0c0c74a18573ceebcd7607a0661fd53d
Andre07G <eleal087@gmail.com>
2025-10-29 00:21:24 -0500

    feat: :sparkles: Crear controlador de solicitudes Se creó el controlador para procesar las operaciones CRUD vinculadas a solicitudes dentro del sistema. by @Andre07g

--- src/controllers/solicitudes_controller.js
+++ src/controllers/solicitudes_controller.js
@@ -1,1 +1,50 @@
-// Inicio
+import { obtenerSolicitudes, obtenerSolicitudPorID, crearSolicitud, actualizarSolicitud, eliminarSolicitud } from "../services/solicitudes_services";
+
+export async function obtenerTodasLasSolicitudes(req, res) {
+    try {
+        const games = await obtenerSolicitudes();
+        res.status(200).json(games);
+    } catch (error) {
+        res.status(500).json({error: "Error al obtener todas las solicitudes"});
+    }
+}
+
+export async function obtenerUnaSolicitudPorID(req, res) {
+    try {
+        const id = req.params.id;
+        const game = await obtenerSolicitudPorID(id);
+        if(!game) return res.status(404).json({error: "Solicitud no encontrada"});
+        res.status(200).json(game)
+    } catch (error) {
+        res.status(500).json({error: "Error al obtener la solicitud"});
+    }
+}
+
+export async function crearUnaSolicitud(req, res) {
+    try {
+        const result = await crearSolicitud(req.body);
+        res.status(201).json(result)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+export async function actualizarUnaSolicitud(req, res) {
+    try {
+        const id = req.params.id;
+        const result = await actualizarSolicitud(id, req.body);
+        res.status(202).json(result);
+    } catch (error) {
+        res.status(404).json({error: error.message})
+    }
+}
+
+export async function eliminarUnaSolicitud(req, res) {
+    try {
+        const id = req.params.id;
+        const result = await eliminarSolicitud(id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({error: error.message});
+    }
+}
