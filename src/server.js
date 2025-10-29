5b4821c48df6d4afe763cbc0ef6adbcc77860728
Andre07G <eleal087@gmail.com>
2025-10-28 20:33:04 -0500

    feat: :rocket: Crear base del servidor Se creó el archivo server.js con la configuración inicial del servidor para futuras pruebas y desarrollo. by @Andre07g

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-// Inicio
+
+//imports
+import express from "express";
+import 'dotenv/config'
+import { conectartBD } from "./config/db.js";
+import cors from 'cors'
+
+//Config
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res)=>{
+    res.status(200).json({message: "Backend on"});
+})
+
+conectartBD().then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Backend listening on http://${process.env.HOST_NAME}:${process.env.PORT}`)
+    })
+})
