54d727f09818f9a37cf7e068cdfd90578524a0ea
Andre07G <eleal087@gmail.com>
2025-10-28 20:46:40 -0500

    feat: :sparkles: Crear DTO de usuarios Se creó el DTO de usuarios para validar y manejar la información de los usuarios registrados. by @Andre07g

--- src/dtos/usuarios_dto.js
+++ src/dtos/usuarios_dto.js
@@ -1,1 +1,51 @@
-// Inicio
+import { body } from "express-validator";
+
+export const createUsuarioDTO = [
+    body("nombre")
+        .isString()
+        .trim()
+        .isLength({ min: 10 })
+        .withMessage("El nombre debe ser string y tener mínimo 10 caracteres"),
+    
+    body("correo")
+        .isEmail()
+        .withMessage("El correo debe ser válido y contener @"),
+
+    body("contraseña")
+        .isString()
+        .isLength({ min: 60 })
+        .withMessage("La contraseña debe ser el hash con mínimo 60 caracteres"),
+
+    body("rol")
+        .isIn(["Admin", "User"])
+        .withMessage("El rol debe ser 'Admin' o 'User'"),
+
+    body("estado")
+        .isIn(["Activo", "Bloqueado"])
+        .withMessage("El estado debe ser 'Activo' o 'Bloqueado'"),
+];
+
+export const updateUsuarioDTO = [
+    body("nombre")
+        .optional()
+        .isString()
+        .trim()
+        .isLength({ min: 10 }),
+
+    body("correo")
+        .optional()
+        .isEmail(),
+
+    body("contraseña")
+        .optional()
+        .isString()
+        .isLength({ min: 60 }),
+
+    body("rol")
+        .optional()
+        .isIn(["Admin", "User"]),
+
+    body("estado")
+        .optional()
+        .isIn(["Activo", "Bloqueado"]),
+];
