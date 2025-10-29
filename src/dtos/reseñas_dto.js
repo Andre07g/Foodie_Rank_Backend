90feffda519e66a3a9f02685052e839176bc4813
Andre07G <eleal087@gmail.com>
2025-10-28 20:46:23 -0500

    feat: :sparkles: Crear DTO de reseñas Se creó el DTO de reseñas para validar los datos de calificaciones y comentarios de los usuarios. by @Andre07g

--- src/dtos/reseñas_dto.js
+++ src/dtos/reseñas_dto.js
@@ -1,1 +1,39 @@
-// Inicio
+import { body } from "express-validator";
+
+export const createReseñaDTO = [
+    body("usuario")
+        .isMongoId()
+        .withMessage("Debe ser un ID de usuario válido"),
+
+    body("restaurante")
+        .isMongoId()
+        .withMessage("Debe ser un ID de restaurante válido"),
+
+    body("calificacion")
+        .isFloat({ min: 1, max: 5 })
+        .withMessage("La calificación debe ser entre 1 y 5"),
+
+    body("comentario")
+        .isString()
+        .trim()
+        .isLength({ min: 1 }),
+
+    body("likes")
+        .optional()
+        .isArray()
+        .withMessage("Likes debe ser un array"),
+
+    body("likes.*")
+        .optional()
+        .isMongoId(),
+
+    body("dislikes")
+        .optional()
+        .isArray(),
+
+    body("dislikes.*")
+        .optional()
+        .isMongoId(),
+];
+
+export const updateReseñaDTO = createReseñaDTO.map(val => val.optional());
