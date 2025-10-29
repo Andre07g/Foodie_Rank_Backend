2dbef7e99bc65a0d89891b434e967cb2e7ad0278
Andre07G <eleal087@gmail.com>
2025-10-29 03:14:21 -0500

    feat: :sparkles: Crear router de usuarios Se creó el router para la gestión de usuarios a través de solicitudes HTTP. by @Andre07g

--- src/routers/usuarios_router.js
+++ src/routers/usuarios_router.js
@@ -1,1 +1,14 @@
-// Inicio
+import  { Router } from "express";
+ import { createUsuarioDTO, updateUsuarioDTO } from "../dtos/usuarios_dto.js";
+ import { validationDTO } from "../middlewares/validationDTO.js";
+import { obtenerTodosLosUsuarios, obtenerUnUsuarioPorID, crearUnUsuario, actualizarUnUsuario, eliminarUnUsuario } from "../controllers/usuarios_controller.js";
+
+const router = Router();
+
+router.get("/", obtenerTodosLosUsuarios);
+router.get("/:id", obtenerUnUsuarioPorID);
+router.post("/", createUsuarioDTO, validationDTO,crearUnUsuario);
+router.patch("/:id", updateUsuarioDTO, validationDTO,actualizarUnUsuario);
+router.delete("/:id", eliminarUnUsuario);
+
+export default router;
