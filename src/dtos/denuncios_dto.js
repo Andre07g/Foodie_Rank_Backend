7ad55af9b176d2b6fd8386f7b3e0e423b99f446b
Andre07G <eleal087@gmail.com>
2025-10-28 20:46:12 -0500

    feat: :sparkles: Crear DTO de denuncios Se creó el DTO de denuncios para gestionar los datos relacionados con reportes o denuncias dentro de la plataforma. by @Andre07g

--- src/dtos/denuncios_dto.js
+++ src/dtos/denuncios_dto.js
@@ -1,1 +1,13 @@
-// Inicio
+import { body } from "express-validator";
+
+export const createDenunciaDTO = [
+    body("reseña")
+        .isMongoId()
+        .withMessage("Debe ser un ObjectId válido"),
+];
+
+export const updateDenunciaDTO = [
+    body("reseña")
+        .optional()
+        .isMongoId()
+];
