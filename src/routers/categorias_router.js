7a00b44721b665b289cc12f79a7ed086a076e37f
Andre07G <eleal087@gmail.com>
2025-10-29 03:13:51 -0500

    feat: :sparkles: Crear router de categorías Se creó el router para gestionar las rutas HTTP relacionadas con categorías. by @Andre07g

--- src/routers/categorias_router.js
+++ src/routers/categorias_router.js
@@ -1,1 +1,13 @@
-// Inicio
+import { Router } from "express";
+ import { createCategoriaDTO,updateCategoriaDTO } from "../dtos/categorias_dto.js";
+ import { validationDTO } from "../middlewares/validationDTO.js";
+import { obtenerTodasLasCategorias, crearUnaCategoria, actualizarUnaCategoria, eliminarUnaCategoria } from "../controllers/categorias.controller.js";
+
+const router = Router();
+
+router.get("/", obtenerTodasLasCategorias);
+router.post("/", createCategoriaDTO, validationDTO,crearUnaCategoria);
+router.patch("/:id", updateCategoriaDTO, validationDTO, actualizarUnaCategoria);
+router.delete("/:id", eliminarUnaCategoria);
+
+export default router;
