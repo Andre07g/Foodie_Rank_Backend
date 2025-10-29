eb6833e4010f61c9d36882b6f7c5af108615400c
Andre07G <eleal087@gmail.com>
2025-10-28 20:46:36 -0500

    feat: :sparkles: Crear DTO de solicitudes Se creó el DTO de solicitudes para gestionar las peticiones externas o internas dentro de la aplicación. by @Andre07g

--- src/dtos/solicitudes_dto.js
+++ src/dtos/solicitudes_dto.js
@@ -1,1 +1,25 @@
-// Inicio
+import { body } from "express-validator";
+
+export const createSolicitudDTO = [
+    body("nombre")
+        .isString()
+        .trim()
+        .notEmpty(),
+
+    body("ubicacion")
+        .isString()
+        .trim()
+        .isLength({ min: 5 }),
+
+    body("imagen")
+        .isString()
+        .trim()
+        .notEmpty(),
+
+    body("popularidad")
+        .isFloat({ min: 1, max: 5 }),
+];
+
+export const updateSolicitudDTO = createSolicitudDTO.map(val =>
+    val.optional()
+);
