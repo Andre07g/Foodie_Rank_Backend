70437b3b0e55574041ca1214a2af2e81254b1c22
Andre07G <eleal087@gmail.com>
2025-10-29 03:14:06 -0500

    feat: :sparkles: Crear router de reseñas Se creó el router para controlar las rutas dedicadas a la administración de reseñas. by @Andre07g

--- src/routers/reseñas_router.js
+++ src/routers/reseñas_router.js
@@ -1,1 +1,14 @@
-// Inicio
+import { Router } from "express";
+ import { createReseñaDTO, updateReseñaDTO } from "../dtos/reseñas_dto.js";
+ import { validationDTO } from "../middlewares/validationDTO.js";
+import { obtenerTodasLasReseñas, obtenerUnaReseñaPorID, crearUnaReseña, actualizarUnaReseña, eliminarUnaReseña } from "../controllers/reseñas_controller.js";
+
+const router = Router();
+
+router.get("/", obtenerTodasLasReseñas);
+router.get("/:id", obtenerUnaReseñaPorID);
+router.post("/", createReseñaDTO, validationDTO,crearUnaReseña);
+router.patch("/:id", updateReseñaDTO, validationDTO,actualizarUnaReseña);
+router.delete("/:id", eliminarUnaReseña);
+
+export default router;
