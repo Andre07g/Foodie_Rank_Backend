a08ac701d35bfa3750bd847462e6e0474aaef582
Andre07g <eleal087@gmail.com>
2025-10-30 12:52:02 -0500

    feat: :page_facing_up: Crear archivo swagger.js Se creó el archivo swagger.js para configurar y gestionar la documentación de la API. by @Andre07g

---
+++ src/swagger.js
@@ -0,0 +1,24 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de Mi Proyecto Express",
+      version: "1.0.0",
+      description: "Documentación generada automáticamente con Swagger y JSDoc",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+        description: "Servidor local de desarrollo",
+      },
+    ],
+  },
+  // Ruta donde Swagger buscará los comentarios de documentación
+  apis: ["./src/routes/*.js"],
+};
+
+export const swaggerSpec = swaggerJSDoc(swaggerOptions);
+export { swaggerUi };
