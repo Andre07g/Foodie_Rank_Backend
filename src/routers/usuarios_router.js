e3b57c78168c565808534791c9b813cfbfbe4664
Andre07g <eleal087@gmail.com>
2025-10-30 09:56:03 -0500

    feat: :sparkles: Actualizar usuario_router.js con nuevas rutas Se agregaron las rutas /login, /logged/admin y /logged/perfil al router de usuarios. by @Andre07g

--- src/routers/usuarios_router.js
+++ src/routers/usuarios_router.js
@@ -1,41 +1,25 @@
 import  { Router } from "express";
- import { createUsuarioDTO, updateUsuarioDTO } from "../dtos/usuarios_dto.js";
- import { validationDTO } from "../middlewares/validationDTO.js";
-import { obtenerTodosLosUsuarios, obtenerUnUsuarioPorID, crearUnUsuario, actualizarUnUsuario, eliminarUnUsuario } from "../controllers/usuarios_controller.js";
+import { createUsuarioDTO, updateUsuarioDTO } from "../dtos/usuarios_dto.js";
+import { validationDTO } from "../middlewares/validationDTO.js";
+import { obtenerTodosLosUsuarios, obtenerUnUsuarioPorID, crearUnUsuario, actualizarUnUsuario, eliminarUnUsuario, iniciarSesion } from "../controllers/usuarios_controller.js";
+import { verificarToken } from "../middlewares/validationTOKEN.js";
+import { verificarAdmin } from "../middlewares/verificarAdmin.js";
 
 const router = Router();
 
 router.get("/", obtenerTodosLosUsuarios);
-router.get("/:id", obtenerUnUsuarioPorID);
+router.get("/buscar/:id", obtenerUnUsuarioPorID);
 router.post("/", createUsuarioDTO, validationDTO,crearUnUsuario);
 router.patch("/:id", updateUsuarioDTO, validationDTO,actualizarUnUsuario);
 router.delete("/:id", eliminarUnUsuario);
-router.post("/login", async (req, res) => {
-  try {
-    const resultado = await login(req.body);
+router.post("/login", iniciarSesion);
 
-    if (!resultado.exito) {
-      return res.status(400).json(resultado);
-    }
+// rutas protegidas
 
-    const usuario = resultado.usuario;
+// user logeado
 
-    // Crear token con info útil
-    const token = jwt.sign(
-      {
-        id: usuario._id,
-        nombre: usuario.nombre,
-        rol: usuario.rol
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
+router.get("/logged/perfil",verificarToken,(req,res)=>{res.json({mensaje:`Bienvenido, ${req.usuario.nombre}`})});
 
-    res.json({ exito: true, usuario, token });
+router.get("/logged/admin",verificarToken,verificarAdmin,(req,res)=>{res.json({mensaje:`Bienvenido, ${req.usuario.nombre}, ${req.usuario.rol}`})})
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ exito: false, mensaje: "Error interno del servidor" });
-  }
-});
 export default router;
